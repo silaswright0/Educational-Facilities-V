7caff8ae2465dc03c56a7e424fa703903ab7283a
carterrows <carter.rows@gmail.com>
2025-11-29 20:04:59 -0500

    edited eslint config

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -89,6 +89,7 @@ module.exports = {
       rules: {
         'no-unused-vars': 'off',
         '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-explicit-any': 'off',
       },
     },
     {
